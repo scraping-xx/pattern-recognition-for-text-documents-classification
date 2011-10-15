615309c481522a70794093a4203adaec83d9a3a4
Andre Dieb Martins <andre.dieb@gmail.com>
2011-10-15 16:46:42 -0300

    nb-train: major refactoring

--- nb-train.py
+++ nb-train.py
@@ -18,9 +18,13 @@ from bag import tokenize, bag_of_words
 db = Connection().theses
 
 def data_valid(doc):
+    """ Returns whether the document has non-empty data.
+    """
     return True if 'data' in doc and len(doc['data']) > 0 else False
 
 def extract_vocabulary(docs):
+    """ Extract whole vocabulary from documents.
+    """
     f = {}
     for doc in docs.find():
         if not data_valid(doc): continue
@@ -34,59 +38,59 @@ def extract_vocabulary(docs):
     cut = (numpy.max(f.values()) + numpy.mean(f.values()))/2.0
     return dict(filter(lambda n: n[1] >= cut, f.iteritems()))
 
-def term_count_naive(vocabulary, textfile):
+def term_count_naive(vocabulary, text):
+    """ Counts all occurrences of vocabulary words on the given text.
+
+    This simplified method simply slides through all the text words
+    accounting for vocabulary words.
+    """
     nterm = dict.fromkeys(vocabulary, 0)
-    for term in textfile.getvalue().split():
+    for term in text.split():
         if term in vocabulary:
             nterm[term] += 1
     return nterm
 
 def train(classes, docs):
+    """ Performs a naive-bayes training for the classes using the given docs.
+    'docs' parameter is expected to be a pymongo collection and classes a
+    list of classes present on the documents.
+
+    Returns a tuple (vocabulary, prior probabilities, conditional probabilities)
+    which defines the classifier (see nb-classify.py).
+    """
     n = docs.count()
     prior = {}
     cond = {}
 
-    print 'Extracting vocabulary...',
     vocabulary = extract_vocabulary(docs)
-    print 'done (d=%d words)' % len(vocabulary)
 
     for cls in classes:
-        print 'Computing prior/conditional probs for class %s' % cls
+        # Compute prior probabilities
         nc = docs.find({'field': cls}).count()
         prior[cls] = nc/float(n)
 
-        print '  Computing term frequency...'
-        nterm = {}
-
-        a = time.time()
-        print '  Joining all class docs...',
+        # Join all documents
         textfile = StringIO.StringIO()
         for doc in docs.find({'field': cls}):
             if not data_valid(doc): continue
             textfile.write((' '.join(tokenize(doc['data'])) + ' ').encode('utf-8', 'replace'))
-        print 'took %.4f secs to write.' % (time.time() - a)
 
-        a = time.time()
-        s = textfile.getvalue()
-        print 'took %.4f secs to retrieve string.' % (time.time() - a)
+        # Count vocabulary occurences on joined documents
+        nterm = term_count_naive(vocabulary, textfile.getvalue())
 
-        #nterm = term_count_bruteforce(vocabulary, textfile)
-        a = time.time()
-        nterm = term_count_naive(vocabulary, textfile)
-        print 'took %.4f secs to count.' % (time.time() - a)
-
-        print '  Computing conditional probabilities...',
+        # Compute conditional probabilities
         for term in vocabulary:
             if term not in cond:
                 cond[term] = {}
             if cls not in cond[term]:
                 cond[term][cls] = {}
             cond[term][cls] = (nterm[term] + 1)/float(sum([t + 1 for t in nterm.values()]))
-        print 'Done'
     return vocabulary, prior, cond
 
-
-if __name__ == '__main__':
+def build_experiment():
+    """ Builds the experiment set by assigning half of the available documents for training
+    and the other half for testing. This is stupid and should be changed.
+    """
     total_docs = db.theses.count()
     training_docs = db.theses.find().limit(int(0.5*total_docs))
 
@@ -96,11 +100,16 @@ if __name__ == '__main__':
     for doc in db.theses.find().skip(int(0.5*total_docs)):
         db.testing.save(doc, safe=True)
 
+if __name__ == '__main__':
+    # Uncomment the following line the first time you run this.
+    #build_experiment()
+
     classes = list(set([d['field'] for d in db.theses.find({}, {'field': 1})]))
+
+    print 'Training...',
     voc, prior, cond = train(classes, db.training)
+    print ' done (vocabulary=%d words)' % len(voc)
 
-    print 'Vocabulary: ', len(voc)
-    print 'Prior:', prior
-    print 'Cond.:', cond
-
+    print 'Saving results to training_result.py...',
     open('training_result.py', 'w').write(('vocabulary=%s\nprior=%s\ncond=%s\n' % (voc, prior, cond)).encode('utf-8', 'replace'))
+    print ' done.'
